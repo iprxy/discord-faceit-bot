686c109515eedfd29de7893279b2727b5951c90a
Artem Kulikov <i@iprxy.me>
2022-03-15 18:27:29 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,7 @@ import textController from './controllers/textController.js'
 const client = new Discord.Client()
 const faceit = new Faceit()
 
+console.log(process.env.DISCORD_TOKEN)
 client.login(process.env.DISCORD_TOKEN)
 client.on('ready', () => client.user.setActivity('Faceit API', { type: 'WATCHING' }))
 client.on('messageReactionAdd', async (reaction) => await reactionController(reaction, faceit))
