d71100355705bcc89cefe980925df1e10cfd9b5e
Artem Kulikov <i@iprxy.me>
2022-03-15 20:29:07 +0300

    bold

--- src/modules/embedTogetherMatches.js
+++ src/modules/embedTogetherMatches.js
@@ -1,13 +1,13 @@
 export default function (username, opponentname,togetherMatches) {
+  const resultString = `**${username}** and **${opponentname}** played **${togetherMatches.length}** matches together`
   let results
-  
   if (togetherMatches.length > 10) {
-    results = `${username} and ${opponentname} played ${togetherMatches.length} matches together, list of last 10 matches:\n` + 
+    results = `${resultString}, list of last 10 matches:\n` + 
       togetherMatches.map(item => `[${item}](https://www.faceit.com/ru/csgo/room/'${item}, '')`).slice(0, 10).join('\n')
   } else if (!togetherMatches.length) {
     results =  `${nickname} never played with ${opponentname}`
   } else {
-    results = `${username} and ${opponentname} played ${togetherMatches.length} matches together:\n` + 
+    results = `${resultString}:\n` + 
       togetherMatches.map(item => `[${item}](https://www.faceit.com/ru/csgo/room/'${item}, '')`).slice(0, 10).join('\n')
   }
   
